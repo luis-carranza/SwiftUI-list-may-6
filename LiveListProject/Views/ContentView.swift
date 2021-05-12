c74f5963294ba23b6216c68063a86eb54c1d37db
Luis Carranza <carranlu@gmail.com>
2021-05-11 21:39:52 -0500

    [FUNCTIONALITY ADDED]  Added Detail View.

--- LiveListProject/Views/ContentView.swift
+++ LiveListProject/Views/ContentView.swift
@@ -13,9 +13,14 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
+    
             List(countryController.countries) { country in
-                Text(country.name)
-            } // List
+                NavigationLink(destination: DetailView(country: country)) {
+                    Text(country.name)
+                }
+                    
+            }
+            
             .navigationBarTitle("Countries", displayMode: .inline)
         } // NavigationView
         
@@ -24,6 +29,9 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+         ContentView()
+        }
     }
 }
+
