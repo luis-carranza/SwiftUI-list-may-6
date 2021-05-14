1b7cae614fc6812bc645055ee247a8222bdae08c
Luis Carranza <carranlu@gmail.com>
2021-05-13 22:24:20 -0500

    [FUNCTION<LITY ADDED]  Added a plus sign using the NavigationBarItems modifier & Created a new View to help us get the information for a  new country.  Regarding the AddCountryView, We included a TextField and an State to store the value of countryName.

--- LiveListProject/Views/ContentView.swift
+++ LiveListProject/Views/ContentView.swift
@@ -13,25 +13,29 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-    
             List(countryController.countries) { country in
                 NavigationLink(destination: DetailView(country: country)) {
                     Text(country.name)
                 }
-                    
             }
-            
             .navigationBarTitle("Countries", displayMode: .inline)
+
+            .navigationBarItems(
+                trailing:NavigationLink(destination: AddCountryView(cityName: "Country")) {
+                    Image(systemName: "plus").foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                }
+            )
+   
         } // NavigationView
-        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-         ContentView()
+        NavigationView{
+          ContentView()
         }
+        
     }
 }
 
